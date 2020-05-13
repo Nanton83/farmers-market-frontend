cf45e45780e78233de00005ec347ec4cfd74935e
nanton83 <nicholas.romans@gmail.com>
2020-05-13 11:16:30 -0400

    creating store in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
 
+//store - storing global data
+// compose combines middleware
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+// dispatching action Object to reducer, reducer decides what to update in our current store and return new version of store
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
+    {/* Provider will make store global to app and any children */}
+
   </React.StrictMode>,
   document.getElementById('root')
 );
