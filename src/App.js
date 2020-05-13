1a31e65d100fd8b2a18e876f5bb3ff4bff103a87
nanton83 <nicholas.romans@gmail.com>
2020-05-13 10:23:32 -0400

    installed json packages, imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 
 class App extends React.Component {
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3001/api/v1/items')
+  //   .then(response => response.json())
+  //   .then(jsondata => console.log(jsondata))
+  // }
+
   render() {
     return (
       <div className="App">
