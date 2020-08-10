47367e9e41c14f27c68332d3a343256a35ca7694
nanton83 <nicholas.romans@gmail.com>
2020-08-10 13:19:17 -0400

    buttons

--- src/components/Markets.js
+++ src/components/Markets.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
-
+import {Button} from 'react-bootstrap'
 import '../styles/App.css';
 // import Counter from '../components/Counter'
 
@@ -13,16 +13,17 @@ const Markets = (props) => {
 
     return (
         <div className="container">
-            <h1 className="centerform">Produce Markets</h1>
-            <div>
+            <h1 className="centerform">Produce Markets</h1><br/>
+            <div class="row">
+            <div class="col-sm-12">
                 {props.markets.map(market => 
                 // pass in key when iterating to avoid warnings
-                <li key={market.id}>
-                    <Link to={`/markets/${market.id}`}>{market.name}</Link> 
-                    {/* - <Counter /> */}
+                
+                <ul key={market.id}>
+                    <Link to={`/markets/${market.id}`}><Button variant="outline-secondary">{market.name}</Button></Link>
                     {/* Any props form click come from route */}
-                    </li> )}
-                {/*  create market component accepting market as props */}
+                    </ul> )}
+            </div>
             </div>
         </div>
     )
